80d790994b799642bb9460a3b4ae8758a35c9efa
Shrutika Asalkar <shrutikaasalkar233@gmail.com>
2024-12-28 11:19:15 +0530

    Update pattern1.java

--- pattern1.java
+++ pattern1.java
@@ -2,7 +2,6 @@ package firstprojectpackage;
 
 public class pattern1 {
 	public static void main(String []args) {
-//		System.out.println("AAAAAA");
 		for(int i = 1; i <= 5; i++) {
 			for(int j = 1; j <= 5; j++) {
 				System.out.print("*");
